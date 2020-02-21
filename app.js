eaa7c16d8bc7dc8c76fd1d658e0a4f27becfe217
StephenS <Stephen.shorrock@gmail.com>
2020-02-21 12:03:04 +0000

    cert file as variable

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 var express = require("express");
-var dotenv = require('dotenv');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
-var saml = require('passport-saml');
+var dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var passport = require("passport");
+var saml = require("passport-saml");
 
 dotenv.load();
 
@@ -18,76 +18,95 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-var samlStrategy = new saml.Strategy({
-  // URL that goes from the Identity Provider -> Service Provider
-  callbackUrl: process.env.CALLBACK_URL,
-  // URL that goes from the Service Provider -> Identity Provider
-  entryPoint: process.env.ENTRY_POINT,
-  // Usually specified as `/shibboleth` from site root
-  issuer: process.env.ISSUER,
-  identifierFormat: null,
-  // Service Provider private key
-  decryptionPvk: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-  // Service Provider Certificate
-  privateCert: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-  // Identity Provider's public key
-  cert: fs.readFileSync(__dirname + '/cert/idp_cert.pem', 'utf8'),
-  validateInResponseTo: false,
-  disableRequestedAuthnContext: true
-}, function(profile, done) {
-  return done(null, profile); 
+var samlStrategy = new saml.Strategy(
+  {
+    // URL that goes from the Identity Provider -> Service Provider
+    callbackUrl: process.env.CALLBACK_URL,
+    // URL that goes from the Service Provider -> Identity Provider
+    entryPoint: process.env.ENTRY_POINT,
+    // Usually specified as `/shibboleth` from site root
+    issuer: process.env.ISSUER,
+    identifierFormat: null,
+    // Service Provider private key
+    decryptionPvk: fs.readFileSync(__dirname + "/cert/key.pem", "utf8"),
+    // Service Provider Certificate
+    privateCert: fs.readFileSync(__dirname + "/cert/key.pem", "utf8"),
+    // Identity Provider's public key
+    cert: fs.readFileSync(__dirname + process.env.IDP_CERT, "utf8"),
+    validateInResponseTo: false,
+    disableRequestedAuthnContext: true
+  },
+  function(profile, done) {
+    return done(null, profile);
+  }
+);
+
+passport.serializeUser(function(user, done) {
+  console.log("passport.serializeUser() user: %s \n", JSON.stringify(user));
+  done(null, user);
 });
 
+passport.deserializeUser(function(user, done) {
+  console.log("passport.deserializeUser() user: %s \n", JSON.stringify(user));
+  done(null, user);
+});
 passport.use(samlStrategy);
 
 var app = express();
 
 app.use(cookieParser());
 app.use(bodyParser());
-app.use(session({secret: process.env.SESSION_SECRET}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  else
-    return res.redirect('/login');
+  if (req.isAuthenticated()) return next();
+  else return res.redirect("/login");
 }
 
-app.get('/',
-  ensureAuthenticated, 
+app.get("/", ensureAuthenticated, function(req, res) {
+  res.send("Authenticated uid:" + JSON.stringify(req.user));
+});
+
+app.get(
+  "/login",
+  passport.authenticate("saml", {
+    failureRedirect: "/login/fail"
+  }),
   function(req, res) {
-    res.send('Authenticated');
+    res.redirect("/");
   }
 );
 
-app.get('/login',
-  passport.authenticate('saml', { failureRedirect: '/login/fail' }),
-  function (req, res) {
-    res.redirect('/');
+app.post(
+  "/login/callback",
+  passport.authenticate("saml", {
+    failureRedirect: "/login/fail"
+  }),
+  function(req, res) {
+    res.redirect("/");
   }
 );
 
-app.post('/login/callback',
-   passport.authenticate('saml', { failureRedirect: '/login/fail' }),
-  function(req, res) {
-    res.redirect('/');
-  }
-);
+app.get("/login/fail", function(req, res) {
+  res.status(401).send("Login failed");
+});
 
-app.get('/login/fail', 
-  function(req, res) {
-    res.status(401).send('Login failed');
-  }
-);
-
-app.get('/Shibboleth.sso/Metadata', 
-  function(req, res) {
-    res.type('application/xml');
-    res.status(200).send(samlStrategy.generateServiceProviderMetadata(fs.readFileSync(__dirname + '/cert/cert.pem', 'utf8')));
-  }
-);
+app.get("/Shibboleth.sso/Metadata", function(req, res) {
+  res.type("application/xml");
+  res
+    .status(200)
+    .send(
+      samlStrategy.generateServiceProviderMetadata(
+        fs.readFileSync(__dirname + "/cert/cert.pem", "utf8")
+      )
+    );
+});
 
 //general error handler
 app.use(function(err, req, res, next) {
@@ -95,7 +114,6 @@ app.use(function(err, req, res, next) {
   next(err);
 });
 
-var server = app.listen(4006, function () {
-  console.log('Listening on port %d', server.address().port)
+var server = app.listen(6001, function() {
+  console.log("Listening on port %d", server.address().port);
 });
-
